462515f201177a6d0082b071bea87cf46b65dc79
camirmas <cjirmas@gmail.com>
2017-01-11 19:15:52 -0800

    Upgrade to Elixir 1.4

--- apps/email/test/email_test.exs
+++ apps/email/test/email_test.exs
@@ -2,7 +2,6 @@ defmodule EmailTest do
   use ExUnit.Case
 
   @digest [%{url: "url", title: "title", subreddit: "subreddit"}]
-  @user %{email: "vader@deathstar.net"}
 
   test "generate_template/2 generates the email template" do
     assert Email.generate_template(@digest)
